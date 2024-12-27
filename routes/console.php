5486aca54cbb0410f8c6946f4c0ea2cac4f0770e
Ben Todd <happynowbtodd@gmail.com>
2024-12-27 10:55:54 -0600

    Make non-static

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+$schedule = new Schedule;
 Schedule::command('odds:fetch-current americanfootball_nfl')->everyFifteenMinutes();
 Schedule::command('odds:fetch-current americanfootball_ncaaf')->everyFifteenMinutes();
 Schedule::command('espn:fetch-fpi')->daily();
