0125b87511870819acadb9fb821902b16104e308
Ben Todd <happynowbtodd@gmail.com>
2024-12-28 09:16:27 -0600

    Fix the scheduler

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-$schedule = new Schedule;
-$schedule->command('odds:fetch-current americanfootball_nfl')->everyFifteenMinutes();
-$schedule->command('odds:fetch-current americanfootball_ncaaf')->everyFifteenMinutes();
-$schedule->command('espn:fetch-fpi')->daily();
-$schedule->command('espn:fetch-college-fpi')->daily();
+app(Schedule::class)->command('odds:fetch-current americanfootball_nfl')->everyFifteenMinutes();
+app(Schedule::class)->command('odds:fetch-current americanfootball_ncaaf')->everyFifteenMinutes();
+app(Schedule::class)->command('espn:fetch-fpi')->daily();
+app(Schedule::class)->command('espn:fetch-college-fpi')->daily();
