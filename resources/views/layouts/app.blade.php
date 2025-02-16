b9cbaf0f729cdb70dfc2fd6be61873598d76d81f
Ben Todd <happynowbtodd@gmail.com>
2025-02-16 16:42:05 -0600

    Add twitter tracking.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,16 @@
     <title>@yield('title', 'Smart Betting Analytics')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    
+    <!-- Twitter conversion tracking base code -->
+    <script>
+    !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+    },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+    a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+    twq('config','ozpqy');
+    </script>
+    <!-- End Twitter conversion tracking base code -->
+
     @include('layouts.partials.posthog')
     @stack('scripts')
 </head>
